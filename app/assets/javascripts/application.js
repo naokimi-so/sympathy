b297902efcb75efed91f2713cc912e2e3c03d689
NaokiSHIMAI <shimai@planningdev.com>
2016-12-26 19:48:21 +0900

    コントローラーを作った

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,23 @@
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+var milkcocoa = MilkCocoa.connectWithApiKey('eggix5vdqzf.mlkcca.com', 'API_Key', 'API_Secret');
+var milkcocoa = new MilkCocoa('eggix5vdqzf.mlkcca.com');
+
+// 'messages'というデータストア名のデータストアを作成（取得）
+var ds = milkcocoa.dataStore('messages');
+
+// titleが'hoge'、contentが'huga'というデータを送信（保存はされない）
+ds.send({title : 'hoge', content : 'huga'});
+// titleが'hoge'、contentが'huga'というデータを送信＆保存
+ds.push({title : 'hoge', content : 'huga'});
+
+ds.on('send', function() {
+  console.log('sendされました！');
+});
+
+ds.on('send', function(sended) {
+  console.log('sendされました！titleは'+sended.value.title+'contentは'+sended.value.content+'です！');
+});
