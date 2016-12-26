054b25b9cf4c773397b9aa6e0ef9774d003f51f5
daijiromaeyama <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2016-12-26 19:46:28 +0900

    create app/models/professor.rb

---
+++ app/models/professor.rb
@@ -0,0 +1,11 @@
+class Professor < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :nickname, presence: true
+
+  has_many :rooms
+
+end
